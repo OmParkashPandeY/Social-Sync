31c7e35497865010aa9f906e0ecbaa1bf9213660
Om Parkash PandeY <ommanglamak47@gmail.com>
2023-11-13 18:24:54 +0530

    Added some modules

---
+++ src/app/views/popups/popups.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NewPostComponent } from './new-post/new-post.component';
+
+// prettier-ignore
+@NgModule({
+  declarations: [
+    NewPostComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PopupsModule { }
